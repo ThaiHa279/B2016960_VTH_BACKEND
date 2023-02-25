3609ac2eef00ff67415ef0062bdf8e005b4af60e
ThaiHa279 <levancuter@gmail.com>
2023-02-25 10:09:44 +0700

    update admin js

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const cors  = require('cors');
 const ApiError = require("./app/api-error");
 const contactsRouter = require("./app/routes/contact.route");
 const app = express();
+const AdminJS = require('adminjs')
+const AdminJSExpress = require('@adminjs/express')
+const session = require('express-session')
+const MongoDB = require("./app/untils/mongodb.util")
+const config = require("./app/config");
+const AdminJSMongoose = require("@adminjs/mongoose");
+const mongoose = require("mongoose");
+
+const PORT = 3001;
+
+const DEFAULT_ADMIN = {
+  email: 'admin@example.com',
+  password: 'password',
+}
+
+const authenticate = async (email, password) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN)
+  }
+  return null
+}
+
 
 app.use(cors())
 app.use(express.json());
@@ -13,13 +35,47 @@ app.get("/",(req, res)=> {
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((req, res, next) => {
-    return next(new ApiError(404, "Resource not found"));
-});
 
-app.use((error, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
-    });
-});
+async function run() {
+
+    const mongooseDB = await mongoose.connect(config.db.uri,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("database connected"))
+    .catch((err) => console.log(err));
+    AdminJS.registerAdapter({
+        Resource: AdminJSMongoose.Resource,
+        Database: AdminJSMongoose.Database
+    })
+    const admin = new AdminJS({})
+    const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+    admin,
+    {
+        authenticate,
+        cookieName: 'adminjs',
+        cookiePassword: 'sessionsecret',
+    },
+    null,
+    {
+        store: mongooseDB,
+        resave: true,
+        saveUninitialized: true,
+        secret: 'sessionsecret',
+        cookie: {
+        httpOnly: process.env.NODE_ENV === 'production',
+        secure: process.env.NODE_ENV === 'production',
+        },
+        name: 'adminjs',
+    }
+    )
+    app.use(admin.options.rootPath, adminRouter)
+    app.listen(PORT, () => {
+        console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
+    })
+}
+
+run();
+
+
 module.exports = app;
